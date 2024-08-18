feae5d70ab216fd60b9b914e10932b9d5b2ac661
Aidre Cabrera <aidrecabrera@gmail.com>
2024-08-19 04:46:41 +0800

    chore: mongodb products and users model

---
+++ src/models/products.ts
@@ -0,0 +1,33 @@
+import {Schema, model} from 'mongoose'
+import {IProduct} from '@/types/products'
+
+const ProductSchema = new Schema<IProduct>({
+  _owner_id: {type: Schema.Types.ObjectId, required: true},
+  manufacturingCostPerUnit: {type: Number},
+  overheadExpenses: [
+    {
+      costPerUnit: {type: Number},
+      expenseCategory: {type: String},
+    },
+  ],
+  productName: {type: String, required: true},
+  productionForecast: {
+    maximumUnitsProducible: {type: Number},
+    productionConstraint: {type: String},
+  },
+  profitMarginSettings: {
+    calculationType: {type: String},
+    percentageValue: {type: Number},
+  },
+  rawMaterials: [
+    {
+      costPerUnit: {type: Number},
+      ingredientName: {type: String},
+      quantityNeededPerUnit: {type: Number},
+      stockOnHand: {type: Number},
+    },
+  ],
+  recommendedRetailPrice: {type: Number},
+})
+
+export const Product = model<IProduct>('Product', ProductSchema)
