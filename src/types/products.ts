baa1ea7ad6121f35884684e1469a238ffbc2f0ec
Aidre Cabrera <aidrecabrera@gmail.com>
2024-08-19 04:46:25 +0800

    chore: types for product and authentication

---
+++ src/types/products.ts
@@ -0,0 +1,37 @@
+import {ObjectId} from 'mongodb'
+
+export interface OverheadExpense {
+  costPerUnit: number
+  expenseCategory: string
+}
+
+export interface ProductionForecast {
+  maximumUnitsProducible: number
+  productionConstraint: string
+}
+
+export interface ProfitMarginSettings {
+  calculationType: string
+  percentageValue: number
+}
+
+export interface RawMaterial {
+  costPerUnit: number
+  ingredientName: string
+  quantityNeededPerUnit: number
+  stockOnHand: number
+}
+
+export interface IProduct extends Document {
+  _owner_id: ObjectId
+  // input
+  productName: string
+  profitMarginSettings?: ProfitMarginSettings
+  // ingredients
+  rawMaterials?: RawMaterial[]
+  overheadExpenses?: OverheadExpense[]
+  manufacturingCostPerUnit?: number
+  // output
+  productionForecast?: ProductionForecast
+  recommendedRetailPrice?: number
+}
