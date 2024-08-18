288d8a3f962e75a2c54a41b20b595ee59bdf9328
Aidre Cabrera <aidrecabrera@gmail.com>
2024-08-19 04:48:13 +0800

    chore: secrets config for authentication

---
+++ src/config/secrets.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_SECRET =
+  process.env.ACCESS_TOKEN_SECRET || 'access-secret-key'
+export const REFRESH_TOKEN_SECRET =
+  process.env.REFRESH_TOKEN_SECRET || 'refresh-secret-key'
